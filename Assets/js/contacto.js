34ae9a56b84ba25e33210d4152c121ead6be8092
Johan Zuluaga <johan16zulu@gmail.com>
2023-02-19 10:11:56 -0500

    petshop terminado

--- Assets/js/contacto.js
+++ Assets/js/contacto.js
@@ -1,3 +1,7 @@
+import { writeSponsors } from './module/functions.js'
+
+const form = document.getElementById('form')
+
 import { getData, createCarruHome,writeSponsorsHome, createShopping} from './module/functions.js'
 
 const carrito = document.getElementById("btn-car")
@@ -6,7 +10,7 @@ const modalCarrito = document.getElementById("modal-content")
 
 let products = JSON.parse(localStorage.getItem("products")) || [] // trae del local storage los productos que fueron agregados al carrito
 let precioTotal = 0
-createShopping(products,shopping,false)
+createShopping(products,shopping,true)
 
 let toys = JSON.parse(localStorage.getItem("toys")) || [] // toma el value de la key "toys" en el localStorage y lo guarda en la variable toys
 let pharmacyProducts = JSON.parse(localStorage.getItem("pharmacyProducts")) || []
@@ -25,7 +29,7 @@ data.then((response) => {
 carrito.addEventListener("click", (e) => {
     precioTotal = 0
     products.forEach(product => precioTotal += product.precio)
-    createShopping(products,shopping,precioTotal,false) // actualiza el modal del carrito
+    createShopping(products,shopping,precioTotal,true) // actualiza el modal del carrito
 
     modalCarrito.addEventListener("click", (e) => {
         if (e.target.className.includes("garbage")) {
@@ -45,7 +49,7 @@ carrito.addEventListener("click", (e) => {
 
             precioTotal = 0
             products.forEach(product => precioTotal += product.precio)
-            createShopping(products,shopping,precioTotal,false) 
+            createShopping(products,shopping,precioTotal,true) 
 
         }else if(e.target.id == "eliminar"){
             cartProducts.forEach(cartProduct => {
@@ -60,13 +64,13 @@ carrito.addEventListener("click", (e) => {
             products = []
             localStorage.setItem("products", JSON.stringify(products))
             precioTotal = 0
-            createShopping(products,shopping,precioTotal,false)
+            createShopping(products,shopping,precioTotal,true)
 
         }else if(e.target.id == "comprar"){
             products = []
             localStorage.setItem("products", JSON.stringify(products))
             precioTotal = 0
-            createShopping(products,shopping,precioTotal,false)
+            createShopping(products,shopping,precioTotal,true)
             Swal.fire({
                 position: 'center',
                 icon: 'success',
@@ -78,18 +82,20 @@ carrito.addEventListener("click", (e) => {
     })
 })
 
-
-
-
-
-let slide = document.getElementById("slide")
-
-let array2 = ["dog.jpg","pexels-adam-kontor-333083.jpg","pexels-kat-smith-551628.jpg","dog-ball.jpg"]
-
-createCarruHome(array2,slide)
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Mensaje Enviado',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    form.reset()
+} );
 
 let slideTrack = document.getElementById("slide-track")
 
-let array = ["dog-chow.png","dog-selection.png","dogui.png","kongo.jpg","pedigree.png","proplan.png","royal-canin.png","sabrocitos.png","whiscas.png","dog-chow.png","dog-selection.png","dogui.png","kongo.jpg","pedigree.png","proplan.png","royal-canin.png","sabrocitos.png","whiscas.png"]
+let array = ["dog-chow.png", "dog-selection.png", "dogui.png", "kongo.jpg", "pedigree.png", "proplan.png", "royal-canin.png", "sabrocitos.png", "whiscas.png", "dog-chow.png", "dog-selection.png", "dogui.png", "kongo.jpg", "pedigree.png", "proplan.png", "royal-canin.png", "sabrocitos.png", "whiscas.png"]
 
-writeSponsorsHome(array, slideTrack)
+writeSponsors(array, slideTrack)
--- Assets/js/module/functions.js
+++ Assets/js/module/functions.js
@@ -15,10 +15,8 @@ export function createCards(list, container, formulario) {
   if (list.length === 0) {
     noEncontrado(container, formulario)
   } else {
-
     for (let element of list) {
       aux += writeCard(element)
-      
     }
     container.innerHTML += aux
   }
@@ -27,16 +25,18 @@ export function createCards(list, container, formulario) {
 function writeCard(element) {
   let color = element.disponibles < 5 ? "red" : "green";
   return `
-    <div class="card producto" id="card" style="width: 18rem;" data-bs-toggle="modal" data-bs-target="#${element._id}">
-      <div>
-        <img src="${element.imagen}"class="card-img-top tam_img_card" alt="${element.producto}">
-        <button class="favorite-btn"><img src="../img/heart.png" class="img-heart" alt="heart"></button>
+    <div class="card producto" id="card" style="width: 18rem;" data-bs-toggle="modal" data-bs-target="#card-${element._id}">
+      <div class="card-img">
+        <img src="${element.imagen}"class="card-img-top tam-img-card" alt="${element.producto}">
+        <button type= "" class="btn-heart">
+        <svg class="icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="black" id="${element.producto}" d="M12.39 20.87a.696.696 0 0 1-.78 0C9.764 19.637 2 14.15 2 8.973c0-6.68 7.85-7.75 10-3.25 2.15-4.5 10-3.43 10 3.25 0 5.178-7.764 10.664-9.61 11.895z" fill="#000000"/></svg>
+        </button>
       </div>
-      <div class="card-body ">
+      <div class="card-body">
         <div class="cont-stock d-flex flex-column">
           <p class="card-text"><b>Precio: $${element.precio}</b></p>
           <p class=" ${color} text-center stock"><b>Stock</b></p>
-          <h5 >${element.producto}</h5>
+          <h5 class="fw-bold">${element.producto}</h5>
         </div>
         <div class="cont-unidad d-flex justify-content-end align-items-end">
           <p>${element.disponibles} Unidades</p>
@@ -44,28 +44,27 @@ function writeCard(element) {
       </div>
     </div>
 
-    <div class="modal fade modal-container" id="${element._id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade modal-container" id="card-${element._id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content w-100 mx-auto">
           <div class="modal-body d-flex justify-content-center gap-3">
             <div class="card" style="width: 25rem;">
               <div>
-                <img src="${element.imagen}"class="card-img-top tam_img_card" alt="${element.producto}">
-                <button class="favorite-btn"><img src="../img/heart.png" class="img-heart" alt="heart"></button>
+                <img src="${element.imagen}"class="card-img-top tam-img-card" alt="${element.producto}">
               </div>
               <div class="card-body">
-                <div class="cont-stock d-flex flex-column">
+                <div class="cont-stock d-flex flex-column align-items-center">
                   <p class="card-text"><b>Precio: $${element.precio}</b></p>
                   <p class=" ${color} text-center stock"><b>Stock</b></p>
-                  <h5 >${element.producto}</h5>
+                  <h5 class="fw-bold text-center">${element.producto}</h5>
                 </div>
-                <div class="cont-unidad d-flex justify-content-end align-items-end">
+                <div class="cont-unidad d-flex justify-content-center align-items-end">
                   <p id="unidades-${element._id}">${element.disponibles} Unidades</p>
                 </div>
               </div>
             </div>
             <div class="d-flex flex-column justify-content-center" style="width: 25rem;">
-              <p>${element.descripcion}</p>
+              <p class="text-center">${element.descripcion}</p>
               <button type="button" class="btn btn-primary" id="${element._id}">Agregar Al Carrito</button>
             </div>      
           </div>
@@ -75,7 +74,6 @@ function writeCard(element) {
   `
 }
 
-
 export function filterProducts(products, value) {
   return products.filter((product) => product.producto.toLowerCase().includes(value))
 }
@@ -84,7 +82,7 @@ export function writeSponsors(list, container) {
   for (let i = 0; i < 18; i++) {
     container.innerHTML += `
         <div class="cont-slide">
-          <img src="../img/${list[i]}" alt="">
+          <img class="border2" src="../img/${list[i]}" alt="">
         </div>
       `
   }
@@ -125,16 +123,16 @@ export function createCarruHome(list, container) {
   container.innerHTML += `
     <div class="carousel-inner">
                       <div class="carousel-item active">
-                        <img src="./Assets/img/${list[0]}" class="d-block w-100 border1" alt="${list[0]}">
+                        <img src="./Assets/img/${list[0]}" class="d-block w-100 m-0 border1" alt="${list[0]}">
                       </div>
                       <div class="carousel-item">
-                        <img src="./Assets/img/${list[1]}" class="d-block w-100 border1" alt="${list[1]}">
+                        <img src="./Assets/img/${list[1]}" class="d-block w-100 m-0 border1" alt="${list[1]}">
                       </div>
                       <div class="carousel-item">
-                        <img src="./Assets/img/${list[2]}" class="d-block w-100 border1" alt="${list[2]}">
+                        <img src="./Assets/img/${list[2]}" class="d-block w-100 m-0 border1" alt="${list[2]}">
                       </div>
                       <div class="carousel-item">
-                        <img src="./Assets/img/${list[3]}" class="d-block w-100 border1" alt="${list[3]}">
+                        <img src="./Assets/img/${list[3]}" class="d-block w-100 m-0 border1" alt="${list[3]}">
                       </div>
                     </div>
   `
@@ -148,50 +146,50 @@ export function noEncontrado(container, formulario) {
   </div>`
 
 }
-
-export function createShopping(container){
-  container.innerHTML =`
-  <button data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="btn-car" ><img src="../img/shopping-cart.png" class="car" alt="car"></button>
-  <div class="modal fade modal-container" id="staticBackdrop" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content w-100 mx-auto">
-                            <div class="modal-body d-flex flex-column justify-content-center align-items-center gap-3">
-                                <div class="d-flex  venta ">
-                                    <img class= "img-cart"src="../img/dog-ball.jpg" alt="ball">
-                                    <div class="shopping-info">
-                                        <div class="cart-info">
-                                            <b>Chunky fgbfgnfn</b>
-                                            <p>precio: 1000</p>
-                                        </div>
-                                        <div class="cart-delete">
-                                            <button class="btn-delete"><img src="../img/garbage.png" class="garbage" alt="garbage"></button>
-                                            <p>5 Unidades</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex  venta ">
-                                    <img class= "img-cart"src="../img/dog-ball.jpg" alt="ball">
-                                    <div class="shopping-info">
-                                        <div class="cart-info">
-                                            <b>Chunky fgbfgnfn</b>
-                                            <p>precio: 1000</p>
-                                        </div>
-                                        <div class="cart-delete">
-                                            <button class="btn-delete"><img src="../img/garbage.png" class="garbage redPath" alt="garbage"></button>
-                                            <p>5 Unidades</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex cart-pago" style="width: 25rem;">
-                                        <p>Total: $12541552</p>
-                                    <div class="btn-pago">
-                                        <button type="button" class="btn btn-primary" ">Comprar</button>
-                                        <button type="button" class="btn btn-danger" ">Eliminar todo</button>
-                                    </div>
-                                </div>      
-                            </div>
-                        </div>
-                    </div>
-                </div>
+//
+/* <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg> */
+export function createShopping(list,container,precioTotal=0,bool){
+  let src = bool ? "../img/garbage.png" : "./Assets/img/garbage.png"
+  let template = ""
+  list.forEach( (element,i) => {
+    template +=`
+      <div class="d-flex venta my-2" id="${i}">
+        <img class= "img-cart" src="${element.imagen}" alt="ball">
+        <div class="shopping-info">
+          <div class="cart-info">
+            <b>${element.producto}</b>
+            <p>Precio: ${element.precio}</p>
+          </div>
+          <div class="cart-delete">
+            <button class="btn-delete"> 
+              <img src="${src}" class="garbage" alt="garbage" id="${element._id}"></img>
+            </button>
+          </div>
+        </div>
+      </div>
+    `
+  })
+  template += `
+    <div class="d-flex cart-pago my-3" style="width: 25rem;">
+      <p class="m-0">Total: $${precioTotal}</p>
+      <div class="btn-pago">
+      <button type="button" class="btn btn-danger" id="eliminar">Eliminar todo</button>
+        <button type="button" class="btn btn-primary" id="comprar">Comprar</button>
+      </div>
+    </div>
   `
+  container.innerHTML = template
+}
+
+export function fillHeart(toys,favoritos,btn){
+  toys.forEach(toy => {
+    favoritos.forEach(fav => {
+        if(toy._id == fav._id){
+            let asd = Array.from(btn).filter(e => e.firstElementChild.children[0].id == toy.producto)
+            asd[0].children[0].children[0].classList.replace("black", "redPath")
+        }
+    })
+})
 }
